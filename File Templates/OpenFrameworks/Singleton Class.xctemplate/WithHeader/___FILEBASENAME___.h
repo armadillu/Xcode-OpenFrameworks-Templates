a5854214b9195bb8805a2973621b90d99e24244f
Oriol Ferrer Mesia <oriol@localprojects.net>
2015-03-30 13:42:43 -0400

    add singleton class

---
+++ File Templates/OpenFrameworks/Singleton Class.xctemplate/WithHeader/___FILEBASENAME___.h
@@ -0,0 +1,29 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "ofMain.h"
+
+class ___FILEBASENAMEASIDENTIFIER___{
+
+public:
+	
+	static ___FILEBASENAMEASIDENTIFIER___& one(){
+		static ___FILEBASENAMEASIDENTIFIER___ instance; // Instantiated on first use.
+		return instance;
+	}
+
+	___FILEBASENAMEASIDENTIFIER___();
+	
+protected:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
