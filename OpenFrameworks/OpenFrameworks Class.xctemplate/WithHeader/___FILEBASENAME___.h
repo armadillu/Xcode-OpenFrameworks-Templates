c76ba61685ea2f68b1e1f32961977df8d7d3065b
Oriol Ferrer Mesia <oriol@localprojects.net>
2015-03-30 13:28:55 -0400

    first

---
+++ OpenFrameworks/OpenFrameworks Class.xctemplate/WithHeader/___FILEBASENAME___.h
@@ -0,0 +1,24 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "ofMain.h"
+
+class ___FILEBASENAMEASIDENTIFIER___{
+
+public:
+	
+	___FILEBASENAMEASIDENTIFIER___();
+	
+protected:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
