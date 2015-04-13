35f38fe3cfb05e43c9bcbe4f09bd5427af0a87a2
Oriol Ferrer Mesia <oriol@localprojects.net>
2015-04-13 11:46:29 -0400

    add ofxInterface Node file template

---
+++ File Templates/OpenFrameworks/ofxInterface Node Class/WithHeader/___FILEBASENAME___.h
@@ -0,0 +1,27 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#ifndef _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+#define _____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____
+
+#include "ofMain.h"
+#include "ofxInterface.h"
+
+using namespace ofxInterface;
+
+class ___FILEBASENAMEASIDENTIFIER___: public Node{
+
+public:
+	
+	___FILEBASENAMEASIDENTIFIER___();
+	
+protected:
+
+};
+
+#endif /* defined(_____PROJECTNAMEASIDENTIFIER________FILEBASENAMEASIDENTIFIER_____) */
